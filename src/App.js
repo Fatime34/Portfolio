f32eefcbbff0d1f269cd9104bb71d9e44e45e0ec
Fatime34 <fqurbanli6102@gmail.com>
2021-12-22 14:16:58 +0400

    Navbar yaradildi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="">
+    <div className="container-fluid">
       
+      <Navbar />
+
     </div>
   );
 }
